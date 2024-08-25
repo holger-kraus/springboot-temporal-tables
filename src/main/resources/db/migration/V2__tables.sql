3eedab63b461d54564d75aa7cae612fdb56b2ba9
Holger Kraus <holger.kraus@innoq.com>
2024-08-25 12:15:25 +0200

    fixed the Id field of the history table.

--- src/main/resources/db/migration/V2__tables.sql
+++ src/main/resources/db/migration/V2__tables.sql
@@ -11,14 +11,14 @@ CREATE TABLE employees
 ALTER TABLE employees  ADD COLUMN sys_period tstzrange NOT NULL DEFAULT tstzrange(current_timestamp, null);
 
 CREATE TRIGGER versioning_trigger BEFORE INSERT OR UPDATE OR DELETE ON employees FOR EACH ROW EXECUTE PROCEDURE versioning('sys_period', 'employees_history', true);
-
-CREATE TABLE employees_history
+create table employees_history
 (
-    id             INTEGER   NOT NULL,
-    name           TEXT      NOT NULL,
-    department     TEXT,
-    salary         DECIMAL(20, 2),
-    mitarbeiter_id BIGINT    NOT NULL,
-    sys_period     TSTZRANGE NOT NULL,
-    CONSTRAINT pk_employees_history PRIMARY KEY (id)
+    id INTEGER generated always as identity
+        constraint pk_employees_history
+            primary key,
+    name           text      not null,
+    department     text,
+    salary         numeric(20, 2),
+    mitarbeiter_id bigint    not null,
+    sys_period     tstzrange not null
 );
